aa0e491f1d25ebe9f4adf45f78ab12e096eb2398
ritvikjain86 <ritvikjain86@hotmail.com>
2017-04-05 02:05:46 +0000

    Update HelloWorld.java

--- HelloWorld/src/com/ritvik/HelloWorld.java
+++ HelloWorld/src/com/ritvik/HelloWorld.java
@@ -3,7 +3,7 @@ package com.ritvik;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Ritvik");
+		System.out.println("Hello Ritvik Jain");
 	}
 
 }
