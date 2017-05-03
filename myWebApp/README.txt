cd2821b225098a5b333d3b264f01764b563c2136
ritvikjain86 <ritvikjain86@hotmail.com>
2017-05-03 10:08:22 +0000

    myWebApp

---
+++ myWebApp/README.txt
@@ -0,0 +1,3 @@
+Profiles
+1. RunApp - Runs the Web Application Via Jetty Plugin
+2. 
---
+++ myWebApp/src/main/java/org/ritvik/TestServlet.java
@@ -0,0 +1,45 @@
+package org.ritvik;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TestDao dao = new TestDao();
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			response.getWriter().append("\nName is: ").append(dao.getData());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
