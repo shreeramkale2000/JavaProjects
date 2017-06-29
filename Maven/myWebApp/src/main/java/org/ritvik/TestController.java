dc8c91ee06e49a19c40b2c985bf1adb8d009928f
ritvikjain86 <ritvikjain86@hotmail.com>
2017-06-29 16:50:56 +0530

    Update TestController.java

--- Maven/myWebApp/src/main/java/org/ritvik/TestController.java
+++ Maven/myWebApp/src/main/java/org/ritvik/TestController.java
@@ -9,13 +9,9 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-@Controller
-@RequestMapping(value = "/TestController")
 public class TestController extends AbstractController {
 	
 	@Autowired
