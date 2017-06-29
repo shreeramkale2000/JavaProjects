6d0d16e2183b4b6143910cadc552581ffa366146
ritvikjain86 <ritvikjain86@hotmail.com>
2017-06-29 15:22:01 +0530

    Update TestController.java

--- Maven/myWebApp/src/main/java/org/ritvik/TestController.java
+++ Maven/myWebApp/src/main/java/org/ritvik/TestController.java
@@ -1,5 +1,7 @@
 package org.ritvik;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,12 +11,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 @Controller
-public class TestController {
+@RequestMapping(value = "/TestController")
+public class TestController extends AbstractController {
 	
 	@Autowired
 	private ApplicationContext appContext;
@@ -24,13 +26,17 @@ public class TestController {
 	
 	private Logger logger = Logger.getLogger(TestController.class);
 
-	@RequestMapping(value = "/TestController", method = RequestMethod.GET)
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
 		logger.info("Entered TestController");
 		TestDaoSpring testDaoSpringImpl = appContext.getBean("testDaoSpringImpl", TestDaoSpring.class);
 		
-		response.getWriter().append(served_msg).append(request.getContextPath());
-		response.getWriter().append("\nName is: ").append(testDaoSpringImpl.getDate());
+		try {
+			response.getWriter().append(served_msg).append(request.getContextPath());
+			response.getWriter().append("\nName is: ").append(testDaoSpringImpl.getDate());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
