5988921c05bd504d03c6085920c9b1c99dd64f12
ritvikjain86 <ritvikjain86@hotmail.com>
2017-06-30 13:58:16 +0530

    Update TestControllerRest.java

--- Maven/myWebApp/src/main/java/org/ritvik/TestControllerRest.java
+++ Maven/myWebApp/src/main/java/org/ritvik/TestControllerRest.java
@@ -1,14 +1,10 @@
 package org.ritvik;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,22 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/TestControllerRest")
 public class TestControllerRest {
 
-	@Autowired
-	private ApplicationContext appContext;
-
 	private Logger logger = Logger.getLogger(TestControllerRest.class);
 
-	@RequestMapping(value = "/Employee", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
-	public ResponseEntity<List<Employee>> getEmployee() {
+	@RequestMapping(value = "/Employee/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Employee> getEmployee(@PathVariable("id") int id) {
 		logger.info("TestControllerRest getEmployee called...");
+		Employee emp = new Employee(id, "Ritvik");
 
-		List<Employee> list = new ArrayList<Employee>();
-		list.add(new Employee(1, "A"));
-		list.add(new Employee(2, "B"));
-		list.add(new Employee(3, "C"));
-
-		logger.info("Employee List is : " + list.toString());
-
-		return new ResponseEntity<List<Employee>>(list, HttpStatus.OK);
+		return new ResponseEntity<Employee>(emp, HttpStatus.OK);
 	}
 }
