f315f525a7d9887be21944a9a6d2d014080e1067
ritvikjain86 <ritvikjain86@hotmail.com>
2017-06-30 13:57:08 +0530

    Update Employee.java

--- Maven/myWebApp/src/main/java/org/ritvik/Employee.java
+++ Maven/myWebApp/src/main/java/org/ritvik/Employee.java
@@ -1,44 +1,30 @@
 package org.ritvik;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "employee")
-@XmlAccessorType(XmlAccessType.NONE)
 public class Employee {
 
 	private int id;
 	private String name;
-
-	public Employee(int id, String name) {
+	
+	public Employee(){
 		super();
+	}
+	
+	public Employee(int id, String name){
 		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	@XmlElement
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@XmlElement
-	public void setName(String name) {
 		this.name = name;
 	}
 	
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + "]";
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
