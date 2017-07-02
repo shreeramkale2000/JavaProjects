04d9f97484f01b7a579c285802ec5fd7409fbe2a
ritvikjain86 <ritvikjain86@hotmail.com>
2017-07-02 23:56:38 +0000

    NEW SERV API

--- Maven/myWebApp/src/main/java/org/ritvik/TestServlet.java
+++ Maven/myWebApp/src/main/java/org/ritvik/TestServlet.java
@@ -7,13 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 import org.apache.log4j.Logger;
 
 /**
  * Servlet implementation class TestServlet
  */
-//@WebServlet("/TestServlet")
+@WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
