171a60a954ebbfadf7648b1d49c8f773c5e65858
ritvikjain86 <ritvikjain86@hotmail.com>
2017-07-03 00:30:43 +0000

    final

--- Maven/myWebApp/src/main/java/org/ritvik/TestControllerModel.java
+++ Maven/myWebApp/src/main/java/org/ritvik/TestControllerModel.java
@@ -1,5 +1,6 @@
 package org.ritvik;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +10,12 @@ import org.springframework.ui.ModelMap;
 @RequestMapping("/TestControllerModel")
 public class TestControllerModel {
 	
+	private Logger logger = Logger.getLogger(TestControllerModel.class);
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
+		logger.info("TestControllerModel printHello called...");
+		
 		model.addAttribute("message", "Hello Spring MVC Framework!");
 		return "hello";
 	}
