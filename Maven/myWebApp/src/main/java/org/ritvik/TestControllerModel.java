6399ef5ceaf73a4bf4d57ded58341074e8a1b8f5
ritvikjain86 <ritvikjain86@hotmail.com>
2017-07-03 00:20:07 +0000

    Hello

---
+++ Maven/myWebApp/src/main/java/org/ritvik/TestControllerModel.java
@@ -0,0 +1,18 @@
+package org.ritvik;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/TestControllerModel")
+public class TestControllerModel {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
+	}
+	
+}
