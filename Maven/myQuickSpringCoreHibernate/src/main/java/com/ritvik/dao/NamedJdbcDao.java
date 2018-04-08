2ef626951bfe5388a8c1300636bac2683a81a28b
Ritvik <Ritvik@Ritvik-Windows>
2018-04-09 01:20:08 +0530

    Hbx Stage 1

--- Maven/myQuickSpringCoreHibernate/src/main/java/com/ritvik/dao/NamedJdbcDao.java
+++
@@ -1,6 +0,0 @@
-package com.ritvik.dao;
-
-public interface NamedJdbcDao {
-	public int getDataCount();
-	public int deleteData(int id);
-}
--- Maven/myQuickSpringCoreHibernate/src/main/java/com/ritvik/spring/BusinessLogic.java
+++ Maven/myQuickSpringCoreHibernate/src/main/java/com/ritvik/spring/BusinessLogic.java
@@ -1,32 +1,15 @@
 package com.ritvik.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
-
-//import com.ritvik.dao.JdbcDao;
-import com.ritvik.dao.JdbcDaoEx;
-import com.ritvik.dao.NamedJdbcDao;
 
 @Repository("businessLogic")
 public class BusinessLogic {
 	
-	//@Autowired
-	//private JdbcDao jdbcDaoImpl;
-	
-	@Autowired
-	private JdbcDaoEx jdbcDaoSupportImpl;
-	
-	@Autowired
-	private NamedJdbcDao namedJdbcDaoImpl;
+	private Logger logger = Logger.getLogger(BusinessLogic.class);
 	
 	public void start() {
-		//jdbcDaoImpl.createTables();
-		
-		namedJdbcDaoImpl.getDataCount();
-		namedJdbcDaoImpl.deleteData(999999999);
-		
-		jdbcDaoSupportImpl.postData(999999999, "051DISCREQ999999999.xml");
-		jdbcDaoSupportImpl.getData(999999999).getName();
+		logger.info("Started");
 	}
 
 }
