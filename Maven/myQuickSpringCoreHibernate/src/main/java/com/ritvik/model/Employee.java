fa9694028480aa45f1ec916bb483f90cd64f75a7
Ritvik <Ritvik@Ritvik-Windows>
2018-04-10 02:37:29 +0530

    Model 1

--- Maven/myQuickSpringCoreHibernate/src/main/java/com/ritvik/model/Employee.java
+++ Maven/myQuickSpringCoreHibernate/src/main/java/com/ritvik/model/Employee.java
@@ -1,8 +1,22 @@
 package com.ritvik.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMP")
 public class Employee {
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
 	private int id;
+	
+	@Column(name = "NAME", length = 50, nullable = false)
 	private String name;
+	
 	public int getId() {
 		return id;
 	}
