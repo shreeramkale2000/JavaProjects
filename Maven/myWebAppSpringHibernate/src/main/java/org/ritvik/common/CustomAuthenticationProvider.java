ee2c0b88cc169dc027c7979979d692f4fe129ddc
Ritvik <Ritvik@Ritvik-Windows>
2018-09-22 22:27:33 +0530

    Spring Security 4

--- Maven/myWebAppSpringHibernate/src/main/java/org/ritvik/common/CustomAuthenticationProvider.java
+++ Maven/myWebAppSpringHibernate/src/main/java/org/ritvik/common/CustomAuthenticationProvider.java
@@ -1,17 +1,30 @@
 package org.ritvik.common;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+	
+	private Logger logger = Logger.getLogger("org.ritvik.processLog");
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		String name = authentication.getName();
+		UsernamePasswordAuthenticationToken token = null;
+		try {
+			logger.info("Key Value - " + authentication.getName());
+			if (authentication.getName().contains("ritvik")) {
+				token = new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials(), new ArrayList<>());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		return new UsernamePasswordAuthenticationToken(name, authentication.getCredentials());
+		return token;
 	}
 
 	@Override
--- Maven/myWebAppSpringHibernate/src/main/java/org/ritvik/controller/HomeController.java
+++ Maven/myWebAppSpringHibernate/src/main/java/org/ritvik/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.ritvik.model.Employee;
 import org.ritvik.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/HomeController")
+@Scope("request")
 public class HomeController {
 	
 	@Autowired
